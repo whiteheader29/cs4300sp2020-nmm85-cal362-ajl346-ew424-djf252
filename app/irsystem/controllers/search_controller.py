a70e4664a5c6cea662e0d0552ffe5589987e4824
nmm85 <nmm85@cornell.edu>
2021-04-05 21:18:56 -0400

    updated names and project name

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,9 +1,9 @@
-from . import *  
+from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 
-project_name = "Ilan's Cool Project Template"
-net_id = "Ilan Filonenko: if56"
+project_name = "Parallel Pigskins: Football Player Similarity Engine"
+net_id = "Neil Madhavani nmm85, Cal Lombardo cal362, Alex Lin ajl346, Eric Whitehead ew424, David Fleurantin djf252"
 
 @irsystem.route('/', methods=['GET'])
 def search():
@@ -15,6 +15,3 @@ def search():
 		output_message = "Your search: " + query
 		data = range(5)
 	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
-
-
-
