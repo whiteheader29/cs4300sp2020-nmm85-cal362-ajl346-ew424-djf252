077ca2c4cc17899fabdeb482329b52924381da4e
whiteheader29 <31511278+whiteheader29@users.noreply.github.com>
2021-04-17 17:51:47 -0400

    Update search_controller.py

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,6 +1,8 @@
 from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
+from flask import current_app
+import json
 
 project_name = "Parallel Pigskins: Football Player Similarity Engine"
 net_id = "Neil Madhavani nmm85, Cal Lombardo cal362, Alex Lin ajl346, Eric Whitehead ew424, David Fleurantin djf252"
@@ -8,9 +10,22 @@ net_id = "Neil Madhavani nmm85, Cal Lombardo cal362, Alex Lin ajl346, Eric White
 @irsystem.route('/', methods=['GET'])
 def search():
 	query = request.args.get('search')
-	if not query:
+	
+	# Right now this is a list, (one of ['p'], ['t'], ['p', 't'], []) - cannot be empty or be both checked
+	checks = request.args.getlist('cbox')	
+
+	# This is a test to see if we can load static json files into our logic - check console to see that it prints
+	# We can store our data (ranking system) as JSONs and then access them upon a query
+	f = current_app.open_resource('static/NFL_team_rosters.json')
+	teams = json.load(f)
+	print("# of teams is: ", len(teams))
+	
+	if not query or len(checks) == 0:
 		data = []
 		output_message = ''
+	elif len(checks) == 2:
+		data = ['']
+		output_message = "Error: two boxes checked. Only check one box."
 	else:
 		output_message = "Your search: " + query
 		data = range(5)
